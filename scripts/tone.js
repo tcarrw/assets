5e8e90ce2444ecbdb8182260e62004d1acee5815
tcarrw <tcarrw@gmail.com>
2025-11-05 17:17:43 -0500

    Create tone.js

---
+++ scripts/tone.js
@@ -0,0 +1,16 @@
+window.IlluminaTones = {
+  marina: 432,
+  dagny: 512,
+  sabrina: 128,
+  dua: 640,
+  ariana: 440,
+  taylor: 528,
+  kendyl: 256,
+  monique: 320,
+  doechii: 480,
+  greta: 196,
+  harbor: 96,
+  hana: 384,
+  aurora: 222,
+  zara: 544
+};
